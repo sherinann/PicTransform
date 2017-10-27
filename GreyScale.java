54d29f35abdd26375bce75e08558fbdad7bd0d1f
sherin ann thomas <sherinannthomas1@gmail.com>
2017-10-27 19:59:26 +0530

    image processing

---
+++ GreyScale.java
@@ -0,0 +1,2 @@
+public class GreyScale {
+}
---
+++ Interface.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Interface extends GreyScale{
+    public Interface(){
+        JFrame frame1=new JFrame();
+        frame1.setTitle("Image Transformer");
+    }
+
+
+    public static void main(String args[]){
+
+    }
+
+}
