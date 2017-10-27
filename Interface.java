94f2767dc1e3701080fb4a95b52492e895ac3c9e
sherin ann thomas <sherinannthomas1@gmail.com>
2017-10-27 21:57:19 +0530

    added file chooser

--- Interface.java
+++ Interface.java
@@ -1,14 +1,41 @@
-import javax.swing.*;
+import javafx.stage.FileChooser;
 
-public class Interface extends GreyScale{
-    public Interface(){
-        JFrame frame1=new JFrame();
+import javax.swing.*;
+import javax.swing.plaf.FileChooserUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Interface extends JFrame implements ActionListener {
+    private JButton Pic;
+    private JFrame frame1;
+    private Interface() {
+        frame1 = new JFrame();
         frame1.setTitle("PicTransform");
+        frame1.setLayout(new FlowLayout());
+        frame1.setSize(400, 400);
+
+        Pic= new JButton("Load Pic");
+        Pic.addActionListener(this);
+
+        frame1.add(Pic);
+
+        frame1.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==Pic){
+            final JFileChooser fc=new JFileChooser();
+            int returnVal= fc.showOpenDialog(this);
+            fc.setVisible(true);
+        }
+
     }
 
 
-    public static void main(String args[]){
 
+
+    public static void main(String args[]){
+        Interface i = new Interface();
     }
 
 }
