ebc0bfa55cfb7a6850708209b921510629ed4e71
sherin ann thomas <sherinannthomas1@gmail.com>
2017-10-28 07:31:25 +0530

    open image

--- Interface.java
+++ Interface.java
@@ -1,14 +1,22 @@
+
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.plaf.FileChooserUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+
 
 public class Interface extends JFrame implements ActionListener {
     private JButton Pic;
     private JFrame frame1;
+    private File f;
     private Interface() {
         frame1 = new JFrame();
         frame1.setTitle("PicTransform");
@@ -24,14 +32,35 @@ public class Interface extends JFrame implements ActionListener {
     }
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==Pic){
-            final JFileChooser fc=new JFileChooser();
-            int returnVal= fc.showOpenDialog(this);
-            fc.setVisible(true);
+            this.ImageSelect();
+            frame1.setVisible(false);
+
         }
 
     }
 
-
+    private void ImageSelect(){
+        JFrame frame2=new JFrame("image");
+        final JFileChooser fc=new JFileChooser();
+        int returnVal= fc.showOpenDialog(this);
+        if(returnVal==JFileChooser.APPROVE_OPTION)
+            f= fc.getSelectedFile();
+        try{
+            BufferedImage im;
+            im= ImageIO.read(new File(f.getAbsolutePath()));
+            ImageIcon ic = new ImageIcon(im);
+            JLabel jb= new JLabel();
+            jb.setIcon(ic);
+            frame2.getContentPane().add(jb,BorderLayout.CENTER);
+            frame2.pack();
+            jb.setVisible(true);
+            fc.setVisible(true);
+            frame2.setVisible(true);
+        }
+        catch(IOException ie){
+            ie.getStackTrace();
+        }
+    }
 
 
     public static void main(String args[]){
